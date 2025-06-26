b29def4a502a80333e08220d1789cc8ea14d469b
Yoyo <Yoyo@BOB-UWU>
2025-06-26 20:29:04 +0100

    Add project files.

---
+++ Finals2.y9/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Finals2.y9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user1 = "Yusuf5";
+            string pass1 = "33kk";
+            string user2 = "Mary80";
+            string pass2 = "af5r";
+
+            string username  = "";
+            string password = "";
+            while((username != user1 || password != pass1) && (username != user2 || password != pass2))
+            {
+                Console.Write("Enter username: ");
+                username = Console.ReadLine();
+                Console.Write("Enter password: ");
+                password = Console.ReadLine();
+                if ((username == user1 && password == pass1) || (username == user2 && password == pass2))
+                {
+                    Console.WriteLine("Access granted");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied");
+                }
+            }
+    }
+}
